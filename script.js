9015c65ad4727a444395b4590a1831e6200c8254
Alexander <alexanderiaconetti@gmail.com>
2021-12-14 16:57:48 -0500

    updated styling

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ const winner = document.getElementById('winner')
 const resetBtn = document.getElementById('reset')
 const winImg = document.getElementById('harold-win')
 const loseImg = document.getElementById('harold-lose')
+const playerImg = document.getElementById('playerChoice')
+const compImg = document.getElementById('compChoice')
 
+const rockImg = "<img src='./rock.png' width=150px>"
+const paperImg = "<img src='./paper.png' width=150px>"
+const sciImg = "<img src='./scissors.png' width=150px>"
 
 let compScore = 0;
 let playerScore = 0;
@@ -26,7 +31,6 @@ weapons.forEach(weapon => {
          let selection = e.target.id;
          let compSelection = computerSelection();
          results.textContent = (playRound(selection, compSelection));
-         console.log(playerScore, compScore);
          player.innerText = `You: ${playerScore}`;
          computer.innerText = `Computer: ${compScore}`;
          winner.innerText = '';
@@ -75,23 +79,45 @@ function playRound(playerSelection, computerSelection){
 
     if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
         playerScore += 1;
+        playerImg.innerHTML = rockImg;
+        compImg.innerHTML = sciImg;
         return "You win! Rock beats Scissors."
     } else if(playerSelection === 'Paper' && computerSelection === 'Rock'){
         playerScore += 1;
+        playerImg.innerHTML = paperImg;
+        compImg.innerHTML = rockImg;
         return "You win! Paper beats Rock."
     } else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
         playerScore += 1;
+        playerImg.innerHTML = sciImg;
+        compImg.innerHTML = paperImg;
         return "You win! Scissors beats Paper."
     } else if(playerSelection === 'Rock' && computerSelection === 'Paper'){
         compScore += 1;
+        playerImg.innerHTML = rockImg;
+        compImg.innerHTML = paperImg;
         return "You lose! Computer chose Paper."
     } else if(playerSelection === 'Paper' && computerSelection === 'Scissors'){
         compScore += 1;
+        playerImg.innerHTML = paperImg;
+        compImg.innerHTML = sciImg;
         return "You lose! Computer chose Scissors."
     } else if(playerSelection === 'Scissors' && computerSelection === 'Rock'){
         compScore += 1;
+        playerImg.innerHTML = sciImg;
+        compImg.innerHTML = rockImg;
         return "You lose! Computer chose Rock."
     } else {
+        if(playerSelection === 'Rock'){
+            playerImg.innerHTML = rockImg;
+            compImg.innerHTML = rockImg;
+        } else if(playerSelection === 'Scissors'){
+            playerImg.innerHTML = sciImg;
+            compImg.innerHTML = sciImg;
+        } else {
+            playerImg.innerHTML = paperImg;
+            compImg.innerHTML = paperImg;
+        }
         return `Tie Game! You both chose ${playerSelection}.`
     }
 }
